d455f31d48dcc76d6beb603fdeca4cf2ad2263d7
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-04-21 17:41:18 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World! on Heroku platform :) ' +
   '<BR>Deploy from Github <b>piotrektekkno/herokuNodeJS </b>');
+  console.log('GET');
+  console.log('Paramety w url: ');
+    var q = req.query;
+    for (var a in q){
+      console.log('Parametr: ' + a + ' wartość: ' +q[a]  );
+    }
+
 });
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
